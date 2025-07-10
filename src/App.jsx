0cbcd3c4b6b277541e37734e0ab78c260ab73402
Saad <31198068+saad-khaan@users.noreply.github.com>
2025-07-10 16:40:21 -0700

    Updated App.jsx and main.jsx to support routing, after i installed react-routouer-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
 import React from "react";
 import Navbar from "./components/Navbar";
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import About from "./pages/About";
+import Map from "./pages/Map";
+import Checklist from "./pages/Checklist";
+import Chatbot from "./pages/Chatbot";
 
 function App() {
   return (
-    <div className="bg-[#0f172a] text-white">
+    <div className="bg-[#0f172a] text-white min-h-screen">
       <Navbar />
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/map" element={<Map />} />
+        <Route path="/checklist" element={<Checklist />} />
+        <Route path="/chatbot" element={<Chatbot />} />
+      </Routes>
     </div>
   );
 }
